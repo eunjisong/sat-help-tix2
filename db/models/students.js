297dcc3b47d100aabcc5540f89f31ec7a79ef933
Sol <sppark95@hotmail.com>
2018-02-01 12:37:40 -0500

    feat: create option definitions describe block for virtuals and hooks, and instance methods for Students

--- db/models/students.js
+++ db/models/students.js
@@ -17,24 +17,24 @@ const Student = db.define('student', {
       isEmail: true
     }
   },
-  // fullName: {
-  //   type: Sequelize.VIRTUAL,
-  //   get() {
-  //     return `${this.getDataValue('firstName')} ${this.getDataValue('lastName')}`;
-  //   }
-  // }
+  fullName: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return `${this.getDataValue('firstName')} ${this.getDataValue('lastName')}`;
+    }
+  }
 }, {
-  // hooks: {
-  // beforeCreate: (student) => {
-  //   student.firstName = `${student.firstName[0].toUpperCase()}${student.firstName.slice(1)}`
-  //   student.lastName = `${student.lastName[0].toUpperCase()}${student.lastName.slice(1)}`
-  //   }
-  // }
+  hooks: {
+  beforeCreate: (student) => {
+    student.firstName = `${student.firstName[0].toUpperCase()}${student.firstName.slice(1)}`
+    student.lastName = `${student.lastName[0].toUpperCase()}${student.lastName.slice(1)}`
+    }
+  }
 });
 
-// Student.prototype.initials = function () {
-//   return `${this.firstName[0]} ${this.lastName[0]}`;
-// }
+Student.prototype.initials = function () {
+  return `${this.firstName[0]} ${this.lastName[0]}`;
+}
 
 module.exports = Student;
 
