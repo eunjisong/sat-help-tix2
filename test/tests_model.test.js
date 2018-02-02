f5077d91f3ca3ba07a56aa11c5189fa6894e73d1
Sol <sppark95@hotmail.com>
2018-02-01 19:57:24 -0500

    feat: add association tests on Test model

--- test/tests_model.test.js
+++ test/tests_model.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-
+const Promise = require('bluebird');
 const Students = require('../db/models/students');
 const Tests = require('../db/models/tests');
 const db = require('../db/db');
@@ -54,5 +54,60 @@ describe('The `Test` model', function() {
     //end of `attributes definition` describe block
   });
 
-  //end of `The Students model` describe block
+  describe('class methods', () => {
+    beforeEach(() => {
+      return Promise.all([
+        Tests.create({ subject: 'Outdoor Survival', grade: 67 }),
+        Tests.create({ subject: 'Competitive Eating', grade: 92 }),
+        Tests.create({ subject: 'Javascript 101', grade: 98 }),
+        Tests.create({ subject: 'Wind Surfing', grade: 52 }),
+        Tests.create({ subject: 'Outdoor Survival', grade: 90 })
+      ]);
+    });
+
+    describe('passing', () => {
+      it('should return the test instances that have grade greater than 70', () => {
+        return Tests.passing().then(foundTests => {
+          expect(foundTests).to.be.an.instanceOf(Array);
+          expect(foundTests).to.have.length(3);
+        });
+      });
+    });
+    describe('findBySubject', () => {
+      it('should return all instances by given subject', () => {
+        return Tests.findBySubject('Outdoor Survival').then(foundTests => {
+          expect(foundTests).to.be.an.instanceOf(Array);
+          expect(foundTests).to.have.length(2);
+        });
+      });
+    });
+  });
+
+  describe('associations', () => {
+    it('belongs to a student', () => {
+      const newStudent = Students.create({
+        firstName: 'Pepper',
+        lastName: 'Potts',
+        email: 'pp@salsa.com'
+      });
+      const newTest = Tests.create({
+        subject: 'sword-sharpening',
+        grade: 100
+      });
+      return Promise.all([newStudent, newTest])
+        .spread((createdStudent, createdTest) => {
+          return createdTest.setStudent(createdStudent);
+        })
+        .then(() => {
+          return Tests.findOne({
+            where: { subject: 'sword-sharpening' },
+            include: { model: Students }
+          });
+        })
+        .then(foundTest => {
+          expect(foundTest).to.exist; //eslint-disable-line no-unused-expressions
+          expect(foundTest.student.fullName).to.equal('Pepper Potts');
+        });
+    });
+  });
 });
